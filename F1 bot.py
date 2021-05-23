2b592dd60e3d4929127d06882a97aa3be464cfb4
InTheArctica <84664004+InTheArctica@users.noreply.github.com>
2021-05-23 04:37:56 -0700

    Add files via upload

---
+++ F1 bot.py
@@ -0,0 +1,9 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> drivers=['KR7', 'AG99', 'PG10', 'YT22', 'EO31', 'FA14', 'SV5', 'LS18', 'CL16', 'CS55', 'DR3', 'LN4', 'NL6', 'GR63', 'MS47', 'MV33', 'SP11', 'VB77', 'LH44']
+>>> x=random.choice(drivers)
+>>> print(x)
+GR63
+>>> 
+>>> 
